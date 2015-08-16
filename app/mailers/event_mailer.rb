4fc95e43a863940cc298e567a6580d4bd3490515
Swami Atma <swami@TenThousandHours.eu>
2015-08-16 13:24:52 +0200

    The event registration confirmation email is sent from organizers if any.

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,12 +1,24 @@
 class EventMailer < ActionMailer::Base
   include Roadie::Rails::Automatic
-  default from: "#{Setting.first.try(:site_name)} <#{Setting.first.contact_email}>"
+  # default from: "#{Setting.first.try(:site_name)} <#{Setting.first.contact_email}>"
 
   def confirmation(user, event)
     @user = user
     @event = event
-    subject = "Registration confirmation"
-    mail to: "#{@user.name} <#{@user.email}>", subject: subject do |format|
+
+    senders = []
+    if event.organizers.any?
+      event.organizers.each do |organizer|
+        senders << "#{organizer.name} <#{organizer.email}>"
+      end
+    else
+      senders << "#{Setting.first.try(:site_name)} <#{Setting.first.contact_email}>"
+    end
+
+    subject = "Registration confirmation for the #{@event.short_name}"
+    mail to: "#{@user.name} <#{@user.email}>",
+          from: senders.join(","),
+          subject: subject do |format|
       format.html { render layout: 'event_public_emails.html.erb' }
       format.text
     end
